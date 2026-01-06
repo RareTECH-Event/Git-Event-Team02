5d2888bf8cd289c90decd04ae251f79cc519e054
YuukiYamano <yuukiyamano1108@gmail.com>
2026-01-06 13:50:10 +0900

    reset

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 def main():
     while True:
         print("選択してください：")
-        print("1: たか@59期")
-        print("2: うっちー@58期2回目")
-        print("3: トンヌラ@55期")
-        print("4: りょうぱん@56期")
+        print("1: 選択肢1")
+        print("2: 選択肢2")
+        print("3: 選択肢3")
         print("q: 終了")
 
         choice = input("> ")
 
         if choice == "1":
-            print("たか@59期が選ばれました。")
+            print("選択肢1が選ばれました。")
         elif choice == "2":
-            print("うっちー@58期2回目が選ばれました。")
+            print("選択肢2が選ばれました。")
         elif choice == "3":
-            print("トンヌラ@55期が選ばれました。")
-        elif choice == "4":
-            print("りょうぱん＠56期が選ばれました。")
+            print("選択肢3が選ばれました。")
         elif choice == "q":
             print("プログラムを終了します。")
             break
@@ -25,4 +22,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
