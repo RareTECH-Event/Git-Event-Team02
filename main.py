a25485373807b99b1ccc90b8fbb79afc85ee6db0
T-Hiro5 <to.hirota12@gmail.com>
2026-01-13 21:49:34 +0900

    main.pyをottiが変更しました

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     while True:
         print("選択してください：")
         print("1: 生大をお願いします")
-        print("2: 選択肢2")
+        print("2: otti")
         print("3: 選択肢3")
         print("q: 終了")
 
@@ -11,7 +11,7 @@ def main():
         if choice == "1":
             print("生大が選ばれました。")
         elif choice == "2":
-            print("選択肢2が選ばれました。")
+            print("otti")
         elif choice == "3":
             print("選択肢3が選ばれました。")
         elif choice == "q":
