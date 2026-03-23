0d0976969aa20bd87e6b5cf9c1d3a9f385919364
ぴょんす <k19951110@yahoo.co.jp>
2026-03-23 22:15:16 +0900

    13行目変更

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
         choice = input("> ")
 
         if choice == "1":
-            print("選択肢1が選ばれました。")
+            print("ぴょんす")
         elif choice == "2":
             print("選択肢2が選ばれました。")
         elif choice == "3":
