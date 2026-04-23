cf089eb83bd9f2150c2b9b2e29e57552680ad4cf
eri <eriron1004@gmail.com>
2026-04-23 21:56:08 +0900

    編集しました

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     while True:
         print("選択してください：")
         print("1: 選択肢1")
-        print("2: 選択肢2")
+        print("2: えり＠62期")
         print("3: 選択肢3")
         print("q: 終了")
 
@@ -11,7 +11,7 @@ def main():
         if choice == "1":
             print("選択肢1が選ばれました。")
         elif choice == "2":
-            print("選択肢2が選ばれました。")
+            print("えりです")
         elif choice == "3":
             print("選択肢3が選ばれました。")
         elif choice == "q":
