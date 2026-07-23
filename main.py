258ba7d62a3bacbf4a212c914bc29de27d87dcb5
ぴょんす <k19951110@yahoo.co.jp>
2026-07-23 21:57:21 +0900

    ぴょんす編集”

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     while True:
         print("選択してください：")
-        print("1: 選択肢1")
+        print("1: ぴょんす")
         print("2: 選択肢2")
         print("3: 選択肢3")
         print("4: 選択肢4")
